abc5e87d7d70c006a0f4cf5433a250ea6d8e6b44
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-09-11 13:33:31 +0200

    Pang v2

--- pang/pang.js
+++ pang/pang.js
@@ -10,8 +10,20 @@ ctx.fillStyle = 'white';
 
 var pos = document.getElementById('pos');
 
-var sound = new Audio();
-sound.src = 'pang.mp3';
+var shoot = new Audio();
+shoot.src = 'pang.mp3';
+
+var hit = new Audio();
+hit.src = 'hit.mp3';
+
+var getRandX = getRoundRandFunc(width);
+var getRandY = getRoundRandFunc(height);
+
+var targetX = getRandX();
+var targetY = getRandY();
+var targetRadius = 50;
+
+console.log(targetX, targetY);
 
 window.addEventListener('mousemove', function(evt) {
   pos.innerHTML = 'X: ' + evt.clientX + ', Y: ' + evt.clientY;
@@ -20,9 +32,39 @@ window.addEventListener('mousemove', function(evt) {
   ctx.fillRect(evt.clientX - 50, evt.clientY - 5, 100, 10);
   ctx.fillRect(evt.clientX - 5, evt.clientY - 50, 10, 100);
   ctx.clearRect(evt.clientX - 10, evt.clientY - 10, 20, 20);
+
+  ctx.beginPath();
+  ctx.arc(targetX, targetY, targetRadius, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.closePath();
 });
 
 window.addEventListener('click', function(evt) {
-  sound.load();
-  sound.play();
+  shoot.load();
+  shoot.play();
+
+  if (mouseInRect(evt.clientX, evt.clientY, targetX - targetRadius, targetY - targetRadius, targetRadius * 2, targetRadius * 2)) {
+    hit.load();
+    hit.play();
+  }
 });
+
+function getRoundRandFunc(max, min) {
+  if (min) {
+    return function() {
+      return Math.round((Math.random() * (max - min)) + min);
+    }
+  } else {
+    return function() {
+      return Math.round(Math.random() * max);
+    };
+  }
+}
+
+function mouseInRect(mX, mY, rX, rY, rW, rH) {
+  if ((mX >= rX && mX <= rX + rW) && (mY >= rY && mY <= rY + rH)) {
+    return true;
+  } else {
+    return false;
+  }
+}
