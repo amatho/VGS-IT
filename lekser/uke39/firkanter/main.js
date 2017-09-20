c637a5ade658ba49fb87deb1df3300afeaada1ee
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-09-20 14:43:55 +0200

    uke39: firkanter prototype

---
+++ lekser/uke39/firkanter/main.js
@@ -0,0 +1,40 @@
+const data = {};
+
+calculateValues();
+
+for (i = 0; i < data.cubes; i++) {
+  const el = document.createElement('div');
+  const div = document.body.appendChild(el);
+  const length = data.randomLength();
+  div.style.width = length + 'px';
+  div.style.height = length + 'px';
+  div.style.backgroundColor = '#ffffff';
+
+  data.intervalId = setInterval(() => {
+    requestAnimationFrame(() => {
+      div.style.transform = `translate(${data.randomX()}px, ${data.randomY()}px)`
+    });
+  }, data.duration);
+}
+
+function calculateValues() {
+  const minLength = Math.round(innerWidth / 50);
+  const maxLength = Math.round(innerWidth / 40);
+
+  data.cubes = Math.ceil(innerWidth / 100);
+  data.duration = 1000;
+  data.randomLength = getRandomIntFunc(minLength, maxLength);
+  data.randomX = getRandomIntFunc(maxLength, innerWidth - maxLength);
+  data.randomY = getRandomIntFunc(maxLength, innerHeight - maxLength);
+}
+
+function getRandomIntFunc(min, max) {
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+
+  return () => {
+    return Math.round((Math.random() * (max - min)) + min);
+  }
+}
