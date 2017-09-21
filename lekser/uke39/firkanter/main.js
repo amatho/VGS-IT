cf463a5a0f8cc628e048653acf744d4500d6023c
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-09-21 12:47:41 +0200

    uke39: styling changes and recalculate button

--- lekser/uke39/firkanter/main.js
+++ lekser/uke39/firkanter/main.js
@@ -2,29 +2,40 @@ const data = {};
 const randDeg = getRandomIntFunc(360);
 
 calculateValues();
+makeCubes();
 
-for (i = 0; i < data.cubes; i++) {
-  const el = document.createElement('div');
-  const div = document.body.appendChild(el);
-  const length = data.randomLength();
-  div.style.width = length + 'px';
-  div.style.height = length + 'px';
-  div.style.backgroundColor = 'hsl(' + randDeg() + ', 100%, 50%)';
+document.getElementById('recalculate').addEventListener('click', evt => {
+  calculateValues();
+  document.querySelectorAll('div').forEach(div => {
+    div.parentElement.removeChild(div);
+  });
+  makeCubes();
+});
 
-  data.intervalId = setInterval(() => {
-    requestAnimationFrame(() => {
-      div.style.transform = `translate(${data.randomX()}px, ${data.randomY()}px)`
-      div.style.backgroundColor = 'hsl(' + randDeg() + ', 100%, 50%)';
-    });
-  }, data.duration);
+function makeCubes() {
+  for (i = 0; i < data.cubes; i++) {
+    const el = document.createElement('div');
+    const div = document.body.appendChild(el);
+    const length = data.randomLength();
+    div.style.width = length + 'px';
+    div.style.height = length + 'px';
+    div.style.backgroundColor = 'hsl(' + randDeg() + ', 100%, 50%)';
+
+    data.intervalId = setInterval(() => {
+      requestAnimationFrame(() => {
+        div.style.transform = `translate(${data.randomX()}px, ${data.randomY()}px)`
+        div.style.backgroundColor = 'hsl(' + randDeg() + ', 100%, 50%)';
+      });
+    }, data.duration);
+  }
 }
 
 function calculateValues() {
   const minLength = Math.round(innerWidth / 50);
   const maxLength = Math.round(innerWidth / 40);
 
-  data.cubes = Math.ceil(innerWidth / 100);
-  data.duration = 1000;
+  data.cubes = 10;
+  data.duration = 2000;
   data.randomLength = getRandomIntFunc(minLength, maxLength);
   data.randomX = getRandomIntFunc(maxLength, innerWidth - maxLength);
   data.randomY = getRandomIntFunc(maxLength, innerHeight - maxLength);
